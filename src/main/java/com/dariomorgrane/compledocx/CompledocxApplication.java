aee465e73e6dfecca2988205f49c772a67cf397c
Dario <daniiletoia@gmail.com>
2022-04-09 17:48:31 +0400

    Initial commit

---
+++ src/main/java/com/dariomorgrane/compledocx/CompledocxApplication.java
@@ -0,0 +1,13 @@
+package com.dariomorgrane.compledocx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompledocxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompledocxApplication.class, args);
+	}
+
+}
